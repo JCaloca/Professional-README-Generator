7dd96c78426d0c15999ff79d7ee4dc2dd423bef7
JCaloca <jacobcaloca1@gmail.com>
2022-10-19 19:16:25 -0700

    Created the inquirer questions object.

--- index.js
+++ index.js
@@ -1,7 +1,71 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  // Project Title
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project's title?",
+  },
+  // Description
+  {
+    type: "input",
+    name: "description",
+    message: "What is your project's description?",
+  },
+  // Installation
+  {
+    type: "input",
+    name: "installation",
+    message: "How do you install your project?",
+  },
+  // Usage
+  {
+    type: "input",
+    name: "usage",
+    message: "What is it's usage?",
+  },
+  // License
+  {
+    type: "checkbox",
+    name: "license",
+    message: "How can you test your project?",
+  },
+  // Contributing
+  {
+    type: "input",
+    name: "contributing",
+    message: "How can others contribute to your project?",
+  },
+  // Testing
+  {
+    type: "input",
+    name: "tests",
+    message: "How can you test your project?",
+  },
+  // Questions
+  {
+    type: "input",
+    name: "questions",
+    message: "How would you like people to contact you for questions?",
+  },
+  // GitHub UserName
+  {
+    type: "input",
+    name: "github",
+    message: "How can you test your project?",
+  },
+  //Email Address
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
